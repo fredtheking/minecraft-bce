b07b691bdea39cf2c940196c9dab3386cbf2c509
JohnLaptop <electromaster469@gmail.com>
2025-09-11 23:03:43 +0300

    init

---
+++ src/pch.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <raylib.h>
+#include <raymath.h>
+#include <tinyfiledialogs.h>
+#include <imgui.h>
+#include <rlImGui.h>
+#include <iostream>
+#include <vector>
+#include <list>
+#include <unordered_set>
+#include <set>
+#include <unordered_map>
+#include <map>
+#include <algorithm>
+#include <ranges>
+#include <string>
+#include <random>
